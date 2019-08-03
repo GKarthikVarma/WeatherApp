5c31db30b7c4bb8fd2f73ce04efc4c8af868366f
Karthik <gkvarma9177@gmail.com>
2019-08-03 17:39:58 -0400

    Autocomplete #1

---
+++ WeatherApp/WeatherApp/BusinessServices/AutoComplete.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.BusinessServices
+{
+    class AutoComplete
+    {
+        private static string API_KEY = "AIzaSyCefDSiPHmfDAdfyPGjz5bdh-5xOg2uWnE";
+        private static string API_BASE = "https://maps.googleapis.com/maps/api/place/autocomplete/";
+
+        public static async Task<List<string>> GoogleAutoComplete(string letter)
+        {
+            WeatherDetails weather = new WeatherDetails();
+            string url = API_BASE + "input=" + letter + "&key" + API_KEY;
+            string result = "";
+            var handler = new HttpClientHandler();
+            HttpClient client = new HttpClient(handler);
+            result = await client.GetStringAsync(url);
+            Console.WriteLine(result);
+            List<String> res = null;
+
+            return res;
+        }
+    }
+
+
+}
